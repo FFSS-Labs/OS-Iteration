53778e058eda1facf30187db184b7ae0707a92bf
Adrian Zywno <adria@MacBook-Air.local>
2023-07-20 19:56:03 -0400

    Theme update

--- client/components/Backgrounds.js
+++ client/components/Backgrounds.js
@@ -1,11 +1,14 @@
 // Background Arrays per palette
 
 import home0 from '../assets/simple-white.jpg';
+import home1 from '../assets/abstract.jpg';
+import signIn1 from '../assets/geo.jpg';
+import addPiece1 from '../assets/flower-print.jpeg';
 
 const bg = {
-  home: [home0, home0, home0, home0],
-  signIn: [home0, home0, home0, home0],
-  addPiece: [home0, home0, home0, home0],
+  home: [home0, home1, home0, home0],
+  signIn: [home0, signIn1, home0, home0],
+  addPiece: [home0, addPiece1, home0, home0],
 };
 
 export default bg;
