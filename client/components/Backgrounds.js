b3676277b82f8c02cc9ccddacfa9803e012a8ad8
Alexander Derritt <aderritt6158@gmail.com>
2023-07-20 18:17:53 -0500

    pallete2 background added

--- client/components/Backgrounds.js
+++ client/components/Backgrounds.js
@@ -1,11 +1,12 @@
 // Background Arrays per palette
 
 import home0 from '../assets/simple-white.jpg';
+import home2 from '../assets/bamboo.jpeg';
 
 const bg = {
-  home: [home0, home0, home0, home0],
-  signIn: [home0, home0, home0, home0],
-  addPiece: [home0, home0, home0, home0],
+  home: [home0, home0, home2, home0],
+  signIn: [home0, home0, home2, home0],
+  addPiece: [home0, home0, home2, home0],
 };
 
 export default bg;
