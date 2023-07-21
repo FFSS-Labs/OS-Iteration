99ce778b081ca642b0f6dc685c51e210ca5b42e1
Steve Schlepphorst <steve.schlepphorst@gmail.com>
2023-07-20 19:18:50 -0500

    final

--- client/components/Backgrounds.js
+++ client/components/Backgrounds.js
@@ -7,12 +7,12 @@ import home0 from '../assets/simple-white.jpg';
 import home1 from '../assets/abstract.jpg';
 import signIn1 from '../assets/geo.jpg';
 import addPiece1 from '../assets/flower-print.jpeg';
+import home2 from '../assets/flower-print.jpeg';
 
 const bg = {
-
   home: [home0, home1, home2, home3],
   signIn: [home0, signIn1, home2, sign3],
-  addPiece: [home0, addPiece1, home2,add3],
+  addPiece: [home0, addPiece1, home2, add3],
 };
 
 export default bg;
